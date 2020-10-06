a315d43f57b820a31d21d74f3b06fc3872270aa5
Sari <sariguci@Saris-MacBook-Pro.local>
2020-10-06 18:38:26 +0200

    lifted up state to app.js

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,34 @@ import React from 'react';
 import './App.css';
 import Todos from '../pages/todos';
 
+class App extends React.Component {
+  state = {
+    todos: [
+      { id: 1, emoji: 'books', todo: 'todo1' },
+      { id: 2, emoji: 'wine', todo: 'todo2' },
+      { id: 3, emoji: 'ball', todo: 'todo3' },
+      { id: 4, emoji: 'apple', todo: 'todo4' }
+    ]
+  };
+
+  handleDelete = todoId => {
+    //console.log("event handler called", todoId);
+    //neues arr erstellen mit allen AUßER dem elem mit der id 
+    const todos = this.state.todos.filter(todo => todo.id !== todoId);
+    //this.setState({ todos: todos });
+    this.setState({ todos });
+  };
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Todo</h1>
-      <Todos />
-    </div>
-  );
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Todo</h1>
+        <Todos todos={this.state.todos} onDelete={this.handleDelete} />
+      </div>
+    );
+  }
 }
 
 export default App;
