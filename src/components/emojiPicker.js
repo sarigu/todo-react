012b012e2749704f51fd1bb68b37ee5a5502e54e
sarigu <32572115+sarigu@users.noreply.github.com>
2020-10-06 19:44:20 +0200

    added form/ todos can be added to list

---
+++ src/components/emojiPicker.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class EmojiPicker extends Component {
+    render() {
+        return (
+            <div>Books</div>
+        );
+    }
+}
+
