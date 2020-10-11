5941af3deb3e8e55ad30a14c64f565730592ea01
Sari <sariguci@Saris-MacBook-Pro.local>
2020-10-11 16:46:29 +0200

    added links

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Nav extends Component {
     render() {
         return (
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
+                    <Link to="/">
+                        <li>Home</li>
+                    </Link>
+                    <Link to="/about">
+                        <li>About</li>
+                    </Link>
                 </ul>
             </nav>
         );
